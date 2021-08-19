517ab053ddde4240b330c098483b1e4b8717be3e
PX <2029098734@qq.com>
2021-08-19 15:24:15 +0800

    complete cortex vectors

--- systick_handler/start.c
+++ systick_handler/start.c
@@ -1,4 +1,5 @@
 #include"sys.h"
+#include"irqn.h"
 
 extern  unsigned int _estack;
 extern  unsigned int __text_end__;
@@ -12,7 +13,7 @@ typedef void (*Handler)(void);
 
 typedef struct{
 	void* estack;
-	Handler vector[15];
+	Handler vector[255];
 } Vectors;
 
 void Reset_Handler(void);
@@ -30,6 +31,55 @@ Vectors cortex_vectors = {
 		// [11] = DebugMon_Handler;
 		// [13] = PendSV_Handler;
 		[14] = SysTick_Handler,
+		[15] = DMA_IRQHandler,
+		[16] = USB_IRQHandler,
+		[17] = USBDMA_IRQHandler,
+		[18] = LCD_IRQHandler,
+		[19] = SCI0_IRQHandler,
+		[20] = UART0_IRQHandler,
+		[21] = UART1_IRQHandler,
+		[22] = SPI0_IRQHandler,
+		[23] = CRYPT0_IRQHandler,
+		[24] = TIM0_0_IRQHandler,
+		[25] = TIM0_1_IRQHandler,
+		[26] = TIM0_2_IRQHandler,
+		[27] = TIM0_3_IRQHandler,
+		[28] = EXTI0_IRQHandler,
+		[29] = EXTI1_IRQHandler,
+		[30] = EXTI2_IRQHandler,
+		[31] = RTC_IRQHandler,
+		[32] = SENSOR_IRQHandler,
+		[33] = TRNG_IRQHandler,
+		[34] = ADC0_IRQHandler,
+		[35] = SSC_IRQHandler,
+		[36] = TIM0_4_IRQHandler,
+		[37] = TIM0_5_IRQHandler,
+		[38] = KBD_IRQHandler,
+		[39] = MSR_IRQHandler,
+		[40] = EXTI3_IRQHandler,
+		[41] = SPI1_IRQHandler,
+		[42] = SPI2_IRQHandler,
+    
+		[44] = SCI2_IRQHandler,
+   
+		[47] = UART2_IRQHandler,
+		[48] = UART3_IRQHandler,
+		
+		[50] = QSPI_IRQHandler,
+		[51] = I2C0_IRQHandler,
+		[52] = EXTI4_IRQHandler,
+		[53] = EXTI5_IRQHandler,
+		[54] = TIM0_6_IRQHandler,
+		[55] = TIM0_7_IRQHandler,
+		
+		[57] = DCMI_IRQHandler,
+   
+		[61] = QR_IRQHandler,
+		[62] = GPU_IRQHandler,
+
+		[64] = AWD_IRQHandler,
+		[65] = DAC_IRQHandler,
+		[66] = SPI5_IRQHandler,
 	},	
 };
 
@@ -43,7 +93,7 @@ void Reset_Handler(void)
 	{
 		for(;ram < &__data_end__;)
 		{
-			*ram = *flash;
+			*ram,flash;
 			flash ++;
 			ram ++;
 		}
--- systick_handler/sys.h
+++ systick_handler/sys.h
@@ -12,6 +12,79 @@
 #define SCB_BASE           		   (0xE000ED00UL)                    /*!< System Control Block Base Address  */
 #define SysTick_BASE  		 	   (0xE000E010UL)                    /*!< SysTick Base Address               */
 
+
+
+typedef enum IRQn
+{
+/******  Cortex-M# Processor Exceptions Numbers ***************************************************/
+
+/* ToDo: use this Cortex interrupt numbers if your device is a CORTEX-M3 / Cortex-M4 device       */
+	NonMaskableInt_IRQn           = -14,      /*!<  2 Non Maskable Interrupt                      */
+	MemoryManagement_IRQn         = -12,      /*!<  4 Memory Management Interrupt                 */
+	BusFault_IRQn                 = -11,      /*!<  5 Bus Fault Interrupt                         */
+	UsageFault_IRQn               = -10,      /*!<  6 Usage Fault Interrupt                       */
+	SVCall_IRQn                   = -5,       /*!< 11 SV Call Interrupt                           */
+	DebugMonitor_IRQn             = -4,       /*!< 12 Debug Monitor Interrupt                     */
+	PendSV_IRQn                   = -2,       /*!< 14 Pend SV Interrupt                           */
+	SysTick_IRQn                  = -1,       /*!< 15 System Tick Interrupt                       */
+
+/******  Device Specific Interrupt Numbers ********************************************************/
+/* ToDo: add here your device specific external interrupt numbers
+         according the interrupt handlers defined in startup_Device.s
+         eg.: Interrupt for Timer#1       TIM1_IRQHandler   ->   TIM1_IRQn                        */
+    DMA_IRQn                                        = 0,
+    USB_IRQn                                        = 1,
+    USBDMA_IRQn                                     = 2,
+    LCD_IRQn                                        = 3,
+    SCI0_IRQn                                       = 4,
+    UART0_IRQn                                      = 5,
+    UART1_IRQn                                      = 6,
+    SPI0_IRQn                                       = 7,
+    CRYPT0_IRQn                                     = 8,
+    TIM0_0_IRQn                                     = 9,
+    TIM0_1_IRQn                                     = 10,
+    TIM0_2_IRQn                                     = 11,
+    TIM0_3_IRQn                                     = 12,
+    EXTI0_IRQn                                      = 13,
+    EXTI1_IRQn                                      = 14,
+    EXTI2_IRQn                                      = 15,
+    RTC_IRQn                                        = 16,
+    SENSOR_IRQn                                     = 17,
+    TRNG_IRQn                                       = 18,
+    ADC0_IRQn                                       = 19,
+    SSC_IRQn                                        = 20,
+    TIM0_4_IRQn                                     = 21,
+    TIM0_5_IRQn                                     = 22,
+    KBD_IRQn                                        = 23,
+    MSR_IRQn                                        = 24,
+    EXTI3_IRQn                                      = 25,
+    SPI1_IRQn                                       = 26,
+    SPI2_IRQn                                       = 27,
+    
+    SCI2_IRQn                                       = 29,
+   
+    UART2_IRQn                                      = 32,
+    UART3_IRQn                                      = 33,
+
+    QSPI_IRQn                                       = 35,
+    I2C0_IRQn                                       = 36,
+    EXTI4_IRQn                                      = 37,
+    EXTI5_IRQn                                      = 38,
+    TIM0_6_IRQn                                     = 39,
+    TIM0_7_IRQn                                     = 40,
+    
+    DCMI_IRQn                                       = 42,
+   
+    QR_IRQn                                         = 46,
+	GPU_IRQn										= 47,
+
+	AWD_IRQn										= 49,
+	DAC_IRQn										= 50,
+	SPI5_IRQn										= 51
+} IRQn_Type;
+
+
+
 typedef struct
 {
     uint32_t FREQ_SEL;
